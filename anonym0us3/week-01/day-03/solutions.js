357b4fb90903e27ccdea47569a837bd0eadac5a8
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-18 23:03:52 -0800

    Finished #s 3 and 4

--- anonym0us3/week-01/day-03/solutions.js
+++ anonym0us3/week-01/day-03/solutions.js
@@ -7,14 +7,26 @@ combineWords( 'dog', 'house' );
 #2 - repeating phrase
 
 function repeatPhrase ( phrase, n ) {
-	for(var i = 0; i < n; i++);
-		console.log( "phrase" );
+	for(var i = 1; i <= n; i++) {
+		console.log( phrase );
+	}
 }
 repeatPhrase( "Hello", 5 );
 
 #3 - power fn
 
 function toTheNthPower( number, power ) {
-	
+	var num = 1;
+	for(i = 1; i <= power; i++) {
+		num *= number;
+	}
+	console.log(num);
 }
 toTheNthPower( 4, 5 );
+
+#4 - circle area
+
+function areaOfACircle ( radius ) {
+	console.log(Math.PI * radius * radius);
+}
+areaOfACircle( 2 );
