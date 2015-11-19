396227bff493b839c3722e430c24c266fc1f4f18
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-18 22:44:07 -0800

    Finished #s 1 and 2

---
+++ anonym0us3/week-01/day-03/solutions.js
@@ -0,0 +1,20 @@
+#1 - concatenation
+function combineWords ( word1, word2 ) {
+	console.log( word1 + word2 );
+}
+combineWords( 'dog', 'house' );
+
+#2 - repeating phrase
+
+function repeatPhrase ( phrase, n ) {
+	for(var i = 0; i < n; i++);
+		console.log( "phrase" );
+}
+repeatPhrase( "Hello", 5 );
+
+#3 - power fn
+
+function toTheNthPower( number, power ) {
+	
+}
+toTheNthPower( 4, 5 );
