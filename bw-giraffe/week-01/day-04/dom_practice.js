4f6efa32a2f4c28b6a1a4dc2e37c3f3c9a0e19b5
bw-giraffe <theletterrsquared@gmail.com>
2015-11-19 19:41:21 -0800

    controlling the dom on Github exercises solutions

---
+++ bw-giraffe/week-01/day-04/dom_practice.js
@@ -0,0 +1,17 @@
+//1
+$('.user-mention').text('anonymous')
+//2
+$('.btn').addClass('btn btn-primary')
+//3 shruggy is so shady with his escape character arm
+$('td.message a.message').text("¯\\_(ツ)_/¯")
+//4
+$('.tooltipped').hide()
+//5
+$('.social-count').text("∞")
+//6
+$(".header-logo-invertocat").html("<img src='https://avatars2.githubusercontent.com/u/4304660?v=3&s=40'>")
+//7
+$('.pagehead-actions').append("<button>release the hounds</button>")
+//8
+$('h1.entry-title').append("/theabyss")
+
