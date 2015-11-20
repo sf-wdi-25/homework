18612be105ce6c62bcf6f6067c7e57f8e3a3e271
Breon Knight <Breonknight7@gmail.com>
2015-11-19 20:55:07 -0800

    first hw

---
+++ breonknight/week-01/day-04/solutions.js
@@ -0,0 +1,8 @@
+$('a.user-mention').text("anonymous");
+$('.btn').addClass('btn-primary');
+$('a.message').text("¯\(ツ)/¯");
+$("img.avatar").hide();
+$(".social-count").text("∞");
+$('.header-logo-invertocat').html("<img src =https://avatars2.githubusercontent.com/u/4304660?v=3&s=40></img>")
+$('ul.pagehead-actions').append('<button> delete repository</button>')
+$('.entry-title').append("H4X0R3d")
