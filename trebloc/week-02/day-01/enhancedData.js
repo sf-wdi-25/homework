12759fbe309f1b2272cec99dd02d39332d4443b2
trebloc <ctse77@alum.berkeley.edu>
2015-11-23 18:13:47 -0800

    Homework Assignment

---
+++ trebloc/week-02/day-01/enhancedData.js
@@ -0,0 +1,62 @@
+var data = {
+    "school": "General Assembly",
+    "address": "225 Bush Street",
+    "city": "San Francisco",
+    "floor": "5th",
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Allen",
+            "first_name": "Nathan",
+            "github_username": "nathanallen"
+        },
+        {
+            "id": 1,
+            "last_name": "Gaff",
+            "first_name": "Travis",
+            "github_username": "tgaff"
+        },
+        {
+            "id": 2,
+            "last_name": "Castilla",
+            "first_name": "Justin",
+            "github_username": "justincastilla"
+        }
+    ],
+
+    "students": [
+        {
+            "id": 0,
+            "last_name": "Tse",
+            "first_name": "Colbert",
+        },
+        {
+            "id": 1,
+            "last_name": "Rowe",
+            "first_name": "Kehontas",
+        },
+        {
+            "id": 2,
+            "last_name": "Chan",
+            "first_name": "Danny",
+        },  
+        {
+            "id": 3,
+            "last_name": "S",
+            "first_name": "Nathan",
+        },  
+        {
+            "id": 4,
+            "last_name": "Tsui",
+            "first_name": "Doug",
+        },                    
+        {
+            "id": 5,
+            "last_name": "Knight",
+            "first_name": "Breon",
+        }    
+        ]    
+}
