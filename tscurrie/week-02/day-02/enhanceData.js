27f98310978929169b3c3b75fd0c93c978407517
tscurrie <tscurrie85@gmail.com>
2015-11-24 09:18:51 -0800

    final

---
+++ tscurrie/week-02/day-02/enhanceData.js
@@ -0,0 +1,50 @@
+//enhanceData.js
+var data = {
+    "school": "General Assembly",
+    "city": "San Francisco",
+    "address" : "225 Bust street",
+    "floor" : "5",
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Nathan",
+            "first_name": "Allen ",
+            "github_username": "nathanallen"
+        },
+        {
+            "id": 1,
+            "last_name": "Travis",
+            "first_name": "Gaff",
+            "github_username": "tgaff"
+        },
+        {
+            "id": 2,
+            "last_name": "Justin",
+            "first_name": "Castilla",
+            "github_username": "justincastilla"
+        }
+    ],
+    "students": [
+        {
+            "id": 3,
+            "last_name": "Currie",
+            "first_name": "Thomas"
+        },
+        {
+            "id": 4,
+            "last_name": "Daniel",
+            "first_name": "Chan"
+            
+        },
+        {
+            "id": 5,
+            "last_name": "Breon",
+            "first_name": "Knight"
+            
+        }
+    ]
+}
+
