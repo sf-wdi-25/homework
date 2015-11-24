54bcc9a3f6f27d533d284d9fd7e10519c5c1d6aa
isangieri <isabelleangieri@gmail.com>
2015-11-24 09:10:16 -0800

    added a few properties and tested working in chrome console

---
+++ IsAngieri/week-02/day-02/enhancedData.js
@@ -0,0 +1,31 @@
+var data = {
+    "school": "General Assembly",
+    "city": "San Francisco",
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Nathan",
+            "first_name": "Allen ",
+            "github_username": "nathanallen",       
+        },
+        {
+            "id": 1,
+            "last_name": "Travis",
+            "first_name": "Gaff",
+            "github_username": "tgaff",
+        },
+        {
+            "id": 2,
+            "last_name": "Justin",
+            "first_name": "Castilla",
+            "github_username": "justincastilla",
+            }
+    ],
+    "address": "bush street",
+    "floor number": 5,
+    "list of students": ["Is","Reva","Colbert","Rich","Kehontas","Alex"]      
+        
+};
