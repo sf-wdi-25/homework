60e1598da7d2c5e4e25464d1c338739b1079bc8e
revalidate <reva.bhatt@gmail.com>
2015-11-23 17:45:48 -0800

    objects challange completed

---
+++ revalidate/week-02/day-01/enhancedData.js
@@ -0,0 +1,45 @@
+var data = {
+    "school": "General Assembly",
+    "city": "San Francisco",
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Nathan",
+            "first_name": "Allen ",
+            "github_username": "nathanallen"
+        },
+        {
+            "id": 1,
+            "last_name": "Travis",
+            "first_name": "Gaff",
+            "github_username": "tgaff"
+        },
+        {
+            "id": 2,
+            "last_name": "Justin",
+            "first_name": "Castilla",
+            "github_username": "justincastilla"
+        }
+    ],
+    "students": [
+        {
+            "id": 3,
+            "last_name": "Roe",
+            "first_name": "Kehontas"
+        },
+        {
+            "id": 4,
+            "last_name": "Chan",
+            "first_name": "Danny"
+        },
+        {
+            "id": 5,
+            "last name": "Bhatt",
+            "first_name": "Reva"
+        }
+    ],
+    "address": "225 Bush Street, San Francisco"
+};
