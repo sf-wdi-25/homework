7ff628d44eb2c275cfd9a33dcc91ffd1ea0f9e62
Joe Roers <Joe@Joe-Roers-MacBook-Pro.local>
2015-11-23 23:47:48 -0800

    added homework for W2D1

---
+++ jroers/week-02/day-01/enhancedData.js
@@ -0,0 +1,60 @@
+var data = {
+    "school": "General Assembly",
+    "city": "San Francisco",
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Nathan",
+            "first_name": "Allen ",
+            "github_username": "nathanallen"
+        },
+        {
+            "id": 1,
+            "last_name": "Travis",
+            "first_name": "Gaff",
+            "github_username": "tgaff"
+        },
+        {
+            "id": 2,
+            "last_name": "Justin",
+            "first_name": "Castilla",
+            "github_username": "justincastilla"
+        }
+    ]
+};
+
+//How many properties in data are Strings?
+    //5 of them - school, city, course, course_id, and classroom
+//how many properties are Arrays?
+    //Just the one - instructors
+//If there is an array, what is the array datatype?
+    //Object
+
+data.address = "225 Bush St.";
+
+data.floor_number = "5";
+
+data.students = [
+    {
+        "first_name": "Joe",
+        "last_name": "Roers",
+        "former_employer": "Epic"
+    },
+    {
+        "first_name": "Kehontas",
+        "last_name": "Rowe",
+        "former_employer": "Wix.com"
+    },
+    {
+        "first_name": "Danny",
+        "last_name": "Chan",
+        "former_employer": "Alpis"
+    },
+    {   "first_name": "Doug",
+        "last_name": "Tsui",
+        "former_employer": "AM Logic"
+    }
+];
