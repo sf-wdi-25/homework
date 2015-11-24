672059645bf7f8b9f84e68d3dc02dd5ef306b231
Breon Knight <Breonknight7@gmail.com>
2015-11-23 21:29:15 -0800

    finshed hw, very informative. Great practice

---
+++ breonknight/week-02/day-01/enhancedData.js
@@ -0,0 +1,61 @@
+var data = {
+    "school": "General Assembly",
+    "address": "225 Bush St",
+    "state": "Ca",
+    "zipcode": "94104", 
+    "city": "San Francisco",
+    "floor": "5", 
+    "course": "Web Development Immersive",
+    "course_id": "WDI25",
+    "classroom": "8",
+    "instructors": [
+        {
+            "id": 0,
+            "last_name": "Nathan",
+            "first_name": "Allen ",
+            "github_username": "nathanallen"
+        },
+        {
+            "id": 1,
+            "last_name": "Travis",
+            "first_name": "Gaff",
+            "github_username": "tgaff"
+        },
+        {
+            "id": 2,
+            "last_name": "Justin",
+            "first_name": "Castilla",
+            "github_username": "justincastilla"
+        }
+    ],
+
+    "students": [
+        {
+            "id": 0,
+            "last_name": "Knight",
+            "first_name": "Breon",
+            "github_username": "breonknight"
+        },
+        {
+            "id": 1,
+            "last_name": "Tsu",
+            "first_name": "Colbert",
+            "github_username": "trebloc"
+        },
+        {
+            "id": 2,
+            "last_name": "Anon",
+            "first_name": "Nathan",
+            "github_username": "anonym0us3"
+        }
+    ]
+}
+
+
+// Able to access data with these lines of code.
+// var gaAddress = data.address;
+// var gaState = data.state;
+// var gaZipCode = data.zipcode;
+// var gaFloor = data.floor;
+// var studentArray = data.students;
+// console.log(studentArray);
